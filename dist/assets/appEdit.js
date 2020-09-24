0973f09764eed30fa5cc2b87312b42b4ef4cbffd
adam allalou <aabou9@gmail.com>
2020-09-24 11:26:30 +0100

    add appEdit file

--- dist/assets/appEdit.js
+++ dist/assets/appEdit.js
@@ -1,4 +1,8 @@
-let reelteaImage = document.querySelector(img[src="dist/assets/images/reeltea.png"])
+let reelteaImage = document.querySelector('img[src="dist/assets/images/reeltea.png"]')
 reelteaImage.addEventListener("mouseenter", () => {
+  reelteaImage.src = 'dist\assets\images\banner.png'; 
+});
+
+reelteaImage.addEventListener("mouseleave", () => {
   reelteaImage.src = 'dist\assets\images\banner.png'; 
 });
