9b8786aec0c5f76e54cadf14b92d25b0db165659
adam allalou <aabou9@gmail.com>
2020-09-24 11:53:16 +0100

    fix appedit slash

--- dist/assets/appEdit.js
+++ dist/assets/appEdit.js
@@ -1,8 +1,8 @@
 let reelteaImage = document.querySelector('img[src="dist/assets/images/reeltea.png"]')
 reelteaImage.addEventListener("mouseenter", () => {
-  reelteaImage.src = 'dist\assets\images\banner.png'; 
+  reelteaImage.src = 'dist/assets/images/banner.png'; 
 });
 
 reelteaImage.addEventListener("mouseleave", () => {
-  reelteaImage.src = 'dist\assets\images\banner.png'; 
+  reelteaImage.src = 'dist/assets/images/banner.png'; 
 });
