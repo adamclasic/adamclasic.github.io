a42b23fa2ad329d2eefa0d40f639f3d7111409f2
adam allalou <aabou9@gmail.com>
2020-09-24 12:00:01 +0100

    add corona game play on hover

--- dist/assets/appEdit.js
+++ dist/assets/appEdit.js
@@ -1,8 +1,17 @@
 let reelteaImage = document.querySelector('img[src="dist/assets/images/reeltea.png"]')
 reelteaImage.addEventListener("mouseenter", () => {
-  reelteaImage.src = 'dist/assets/images/banner.png'; 
+  reelteaImage.src = 'dist/assets/images/reeltea-screenshot.png'; 
 });
 
 reelteaImage.addEventListener("mouseleave", () => {
-  reelteaImage.src = 'dist/assets/images/banner.png'; 
+  reelteaImage.src = 'dist/assets/images/reeltea.png'; 
+});
+
+let CoronaImage = document.querySelector('img[src="dist/assets/images/feature.png"]')
+reelteaImage.addEventListener("mouseenter", () => {
+  reelteaImage.src = 'dist/assets/images/gameplay.png'; 
+});
+
+reelteaImage.addEventListener("mouseleave", () => {
+  reelteaImage.src = 'dist/assets/images/feature.png'; 
 });
